a7b3556d0d5c62b895f7cbbbb1c54d07d8e4bbe8
Francesco <francesco.savignano96@gmail.com>
2020-11-23 15:12:28 +0100

    cards generate

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 var app = new Vue({
     el: '#root',
     data: {
-
+        cds: []
     },
 
     mounted() {
         const self = this;
 
         axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .get('https://flynn.boolean.careers/exercises/api/array/music')
             .then(function(risposta) {
-
-
+                self.cds = risposta.data.response;
             });
 
     }
