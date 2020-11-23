1f6bd426d7e56284c10ef6298831445a85725c19
Francesco <francesco.savignano96@gmail.com>
2020-11-23 19:09:31 +0100

    array ordinato a seconda degli anni tramite array.sort(function(a,b){});

--- main.js
+++ main.js
@@ -14,34 +14,10 @@ var app = new Vue({
         },
         //funzione che prende come parametro l'array di cd e lo riordina in base al valore della proprietà anno degli oggetti
         sortByYear(cds_array){
-            //variabile che, se vera, indica che sono stati cambiati dei valori per ordinare l'array
-            let flag;
 
-            do{
-
-                //variabile che ad ogni giro del do/while viene impostata a false, per indicare che non ci sono stati scambi fino ad ora
-                flag = false;
-
-                //scorro l'array confrontando le coppie di oggetti dell'array in posizione i e i-1
-                for (let i = 1; i < cds_array.length; i++) {
-
-                    //se l'anno dell'oggetto in i è maggiore dell'anno dell'oggetto precedente, scambia di posizione la coppia di oggetti e imposta flag=true
-                    if (parseInt(this.cds[i].year) < parseInt(this.cds[i - 1].year)) {
-
-                        let tmp = this.cds[i - 1];
-                        this.cds[i - 1] = this.cds[i];
-                        this.cds[i] = tmp;
-
-                        flag = true;
-
-                    }
-
-                }
-
-            //ripeti se ci sono stati scambi di oggetti nell'array
-            }while(flag == true);
-
-            //ricarica la pagina con l'array ordinato
+            cds_array.sort(function(a, b) {
+                return a.year - b.year;
+            });
             app.$forceUpdate();
 
         }
