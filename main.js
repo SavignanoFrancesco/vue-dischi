41ae1eaa05b6cea9755b5aceac1191ed14dd98b8
Francesco <francesco.savignano96@gmail.com>
2020-11-23 19:53:58 +0100

    sostituiti i bottoni per la scelta dell'ordine con una select

--- main.js
+++ main.js
@@ -4,13 +4,25 @@ var app = new Vue({
         //array che conterrà la risposta generata dall'API
         cds: [],
         //variabile contenente il valore del select
-        genre_selected: 'All'
+        genre_selected: 'All',
+        //
+        order_selected: 'Unsorted'
     },
     methods: {
         //funzione che passa il valore selezionato nella pagina alla variabile genre_selected
         genreChange(value){
             // console.log(value);
             this.genre_selected = value;
+        },
+        //
+        sortChange(value){
+            if (value == 'Unsorted') {
+                this.unsortedCds();
+            }else if (value == 'Ascending') {
+                this.sortByYear(this.cds)
+            }else if (value == 'Descending') {
+                this.sortByYearDescending(this.cds)
+            }
         },
         //funzione che prende come parametro l'array di cd e lo riordina in base al valore della proprietà anno degli oggetti
         sortByYear(cds_array){
