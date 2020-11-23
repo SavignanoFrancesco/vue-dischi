fe9dc1357f147c5de4cc6ede4cfde04df72747a6
Francesco <francesco.savignano96@gmail.com>
2020-11-23 16:10:22 +0100

    commentato codice js e sostituito ciclo while con ciclo do-while nella funzione di ordinamento

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
 var app = new Vue({
     el: '#root',
     data: {
+        //array che conterrà la risposta generata dall'API
         cds: [],
+        //variabile contenente il valore del select
         genre_selected: 'All'
     },
     methods: {
+        //funzione che passa il valore selezionato nella pagina alla variabile genre_selected
         genreChange(value){
             // console.log(value);
             this.genre_selected = value;
         },
+        //funzione che prende come parametro l'array di cd e lo riordina in base al valore della proprietà anno degli oggetti
         sortByYear(cds_array){
-            let flag = true;
-            while(flag == true){
+            //variabile che, se vera, indica che sono stati cambiati dei valori per ordinare l'array
+            let flag;
+
+            do{
+
+                //variabile che ad ogni giro del do/while viene impostata a false, per indicare che non ci sono stati scambi fino ad ora
                 flag = false;
+
+                //scorro l'array confrontando le coppie di oggetti dell'array in posizione i e i-1
                 for (let i = 1; i < cds_array.length; i++) {
 
+                    //se l'anno dell'oggetto in i è maggiore dell'anno dell'oggetto precedente, scambia di posizione la coppia di oggetti e imposta flag=true
                     if (parseInt(this.cds[i].year) < parseInt(this.cds[i - 1].year)) {
 
                         let tmp = this.cds[i - 1].year;
@@ -26,7 +37,9 @@ var app = new Vue({
                     }
 
                 }
-            }
+
+            //ripeti se ci sono stati scambi di oggetti nell'array
+            }while(flag == true);
 
         }
     },
@@ -41,8 +54,6 @@ var app = new Vue({
                 self.sortByYear(self.cds);
                 console.log(self.cds);
             });
-
-
 
     }
 });
