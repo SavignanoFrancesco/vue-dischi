2dfdec6e5b1befe4c7663acc8debf0323827a6d7
Francesco <francesco.savignano96@gmail.com>
2020-11-23 15:54:18 +0100

    array dei dischi ordinato tramite funzione sortByYear(array_dei_cd))

--- main.js
+++ main.js
@@ -8,6 +8,26 @@ var app = new Vue({
         genreChange(value){
             // console.log(value);
             this.genre_selected = value;
+        },
+        sortByYear(cds_array){
+            let flag = true;
+            while(flag == true){
+                flag = false;
+                for (let i = 1; i < cds_array.length; i++) {
+
+                    if (parseInt(this.cds[i].year) < parseInt(this.cds[i - 1].year)) {
+
+                        let tmp = this.cds[i - 1].year;
+                        this.cds[i - 1].year = this.cds[i].year;
+                        this.cds[i].year = tmp;
+
+                        flag = true;
+
+                    }
+
+                }
+            }
+
         }
     },
     mounted() {
@@ -16,8 +36,13 @@ var app = new Vue({
         axios
             .get('https://flynn.boolean.careers/exercises/api/array/music')
             .then(function(risposta) {
+
                 self.cds = risposta.data.response;
+                self.sortByYear(self.cds);
+                console.log(self.cds);
             });
+
+
 
     }
 });
